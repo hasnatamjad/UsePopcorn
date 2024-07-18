60aa0014b97d0bc2a36d1a54d5b7f7e6b68dde13
Muhammad Hasnat <mhasnat647@gmail.com>
2024-07-19 01:46:44 +0500

    creating star component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
+
+function TestRating() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setMovieRating} />
+      <p>This movie got {movieRating} rating</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={7} color={"#000"} />
+    <StarRating maxRating={10} defaultRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["bad", "Ok", "Good", "Excellent"]}
+      defaultRating={3}
+    />
+    <TestRating />
   </React.StrictMode>
 );
